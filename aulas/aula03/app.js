0d543c852e5740dd4a2a68d7ac17d0f98ff7c627
Rihanna Gabriella <rihannafsantos@gmail.com>
2026-02-24 11:12:14 -0300

    feat: add aula03js

---
+++ aulas/aula03/app.js
@@ -0,0 +1,11 @@
+         let nome = "Rihanna";
+        let idade = 17;
+        let cidade = "São Paulo";
+        let n1 = 24;
+        let n2 = 8;
+
+        console.log(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}`)
+        console.log(`Soma: ${n1 + n2}`);
+        console.log(`Subtração: ${n1 - n2}`);
+        console.log(`Multiplicação: ${n1 * n2}`);
+        console.log(`Divisão: ${n1 / n2}`); 
